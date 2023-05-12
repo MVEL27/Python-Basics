8e595c1f590f09ecee903054dfffcccf0a6b9e5c
Murugavel <133084162+MVEL27@users.noreply.github.com>
2023-05-12 14:04:55 +0530

    Add files via upload

---
+++ Arithmetic, short hand & comparision operator.py
@@ -0,0 +1,285 @@
+Python 3.11.3 (v3.11.3:f3909b8bc8, Apr  4 2023, 20:12:10) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+help()
+
+Welcome to Python 3.11's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the internet at https://docs.python.org/3.11/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> keywords
+
+Here is a list of the Python keywords.  Enter any keyword to get more help.
+
+False               class               from                or
+None                continue            global              pass
+True                def                 if                  raise
+and                 del                 import              return
+as                  elif                in                  try
+assert              else                is                  while
+async               except              lambda              with
+await               finally             nonlocal            yield
+break               for                 not                 
+
+help> identifiers
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    help()
+  File "<frozen _sitebuiltins>", line 103, in __call__
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2007, in __call__
+    self.interact()
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2034, in interact
+    self.help(request)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2060, in help
+    elif request: doc(request, 'Help on %s:', output=self._output)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1784, in doc
+    pager(render_doc(thing, title, forceload))
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1758, in render_doc
+    object, name = resolve(thing, forceload)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1744, in resolve
+    raise ImportError('''\
+ImportError: No Python documentation found for 'identifiers'.
+Use help() to get the interactive help utility.
+Use help(str) for help on the str class.
+help()
+
+Welcome to Python 3.11's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the internet at https://docs.python.org/3.11/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> quit
+
+You are now leaving help and returning to the Python interpreter.
+If you want to ask for help on a particular object directly from the
+interpreter, you can type "help(object)".  Executing "help('string')"
+has the same effect as typing a particular string at the help> prompt.
+# Arithmetic operations
+help()
+
+Welcome to Python 3.11's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the internet at https://docs.python.org/3.11/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> arithmetic operators
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    help()
+  File "<frozen _sitebuiltins>", line 103, in __call__
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2007, in __call__
+    self.interact()
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2034, in interact
+    self.help(request)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2060, in help
+    elif request: doc(request, 'Help on %s:', output=self._output)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1784, in doc
+    pager(render_doc(thing, title, forceload))
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1758, in render_doc
+    object, name = resolve(thing, forceload)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1744, in resolve
+    raise ImportError('''\
+ImportError: No Python documentation found for 'arithmetic operators'.
+Use help() to get the interactive help utility.
+Use help(str) for help on the str class.
+quit
+Use quit() or Ctrl-D (end-of-file) to exit
+a=187
+b=785
+c=924
+z=a+b+c
+z
+1896
+z/6
+316.0
+z-96
+1800
+z*10
+18960
+z%9
+6
+help()
+
+Welcome to Python 3.11's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the internet at https://docs.python.org/3.11/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> remainder
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    help()
+  File "<frozen _sitebuiltins>", line 103, in __call__
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2007, in __call__
+    self.interact()
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2034, in interact
+    self.help(request)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2060, in help
+    elif request: doc(request, 'Help on %s:', output=self._output)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1784, in doc
+    pager(render_doc(thing, title, forceload))
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1758, in render_doc
+    object, name = resolve(thing, forceload)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1744, in resolve
+    raise ImportError('''\
+ImportError: No Python documentation found for 'remainder'.
+Use help() to get the interactive help utility.
+Use help(str) for help on the str class.
+y=19
+y%2
+1
+y%7
+5
+85**2
+7225
+95**2
+9025
+4**3
+64
+y%4
+3
+help()
+
+Welcome to Python 3.11's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the internet at https://docs.python.org/3.11/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> floor division operator
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    help()
+  File "<frozen _sitebuiltins>", line 103, in __call__
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2007, in __call__
+    self.interact()
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2034, in interact
+    self.help(request)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 2060, in help
+    elif request: doc(request, 'Help on %s:', output=self._output)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1784, in doc
+    pager(render_doc(thing, title, forceload))
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1758, in render_doc
+    object, name = resolve(thing, forceload)
+  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/pydoc.py", line 1744, in resolve
+    raise ImportError('''\
+ImportError: No Python documentation found for 'floor division operator'.
+Use help() to get the interactive help utility.
+Use help(str) for help on the str class.
+# floor division operatoer - the floor division is the division in which whole part of the result is given & the fractional part is truncated.
+y/2
+9.5
+floor divion = y//2
+SyntaxError: invalid syntax
+floor division = y//2
+SyntaxError: invalid syntax
+y//2
+9
+16.4/2
+8.2
+16.4//2
+8.0
+
+#short hand arithmetic operations
+# it can also be called as augmented arithmetic operators, which combines the impact of arithmetic opertor with augmented operator
+
++= -= /= //= *= **= %=
+SyntaxError: invalid syntax
+z=15
+z+=15
+z
+30
+z+=(-15)
+z
+15
+z
+15
+z-=15
+z
+0
+>>> z-=-15
+>>> z
+15
+>>> z+=15
+>>> z
+30
+>>> z/=2
+>>> z
+15.0
+>>> z//=2
+>>> z
+7.0
+>>> z+=8
+>>> z
+15.0
+>>> z//=4
+>>> z
+3.0
+>>> z*=3
+>>> z
+9.0
+>>> z**=3
+>>> z
+729.0
+>>> # Relational operators - is the relationships the value/variable can have with one another. thus relational operators determines the relation among operands.
+>>> # < <= > >= == !=
+>>> x=9
+>>> y=8.5
+>>> x>y
+True
+>>> x>=y
+True
+>>> x<y
+False
+>>> x<=y
+False
+>>> x==y
+False
+>>> x!=y
+True
+>>> x>=12
+False
